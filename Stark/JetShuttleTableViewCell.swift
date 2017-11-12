a14206d875b02a87b2bc8f434d3a1e480a19d880
Chris David <chrisrdavid7@gmail.com>
2017-11-11 20:56:08 -0500

    first Commit

---
+++ Stark/JetShuttleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  JetShuttleTableViewCell.swift
+//  Stark
+//
+//  Created by Chris David on 11/11/17.
+//  Copyright © 2017 Stark. All rights reserved.
+//
+
+import UIKit
+
+class JetShuttleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
